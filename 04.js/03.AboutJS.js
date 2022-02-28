a0a02db579e889546fc7dde318597d9ab29c7c10
YutaAsai <100548838+YutaAsai@users.noreply.github.com>
2022-02-28 18:34:51 +0900

    Delete 03.AboutJS.js

--- 04.js/03.AboutJS.js
+++
@@ -1,5 +0,0 @@
-// 編集してみる
-const AboutContentContainer = document.getElementById("AboutContentContainer");
-const IconPerson = document.getElementById("IconPerson");
-const AboutText = document.getElementById("AboutText");
-const AboutTextName = document.getElementById("AboutTextName");
